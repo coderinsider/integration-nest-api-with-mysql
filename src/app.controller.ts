de48bba042a4b2c84cb20d321000f1ae6585def7
Myat Ko Ko <coderinsider1@gmail.com>
2022-03-06 19:15:17 +0630

    Adding about, about detail, and about API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -12,13 +13,50 @@ export class AppController {
 
    @Get('/about')
    @Render('index')
-   root() {
+   about() {
       const pageLists = {
          message: "I love you so much, my dream girls",
          pageTitle: 'Nest Application',
          currentPage: 'nest-application'
       }
       return pageLists
+   }
+
+   @Get('/about/detail')
+   aboutdetail(@Res() res: Response) {
+      return res.render(
+         'index',
+         {
+            pageTitle: 'Nest Application',
+            currentPage: 'nest-application',
+            message: 'I am working on sg'
+         }
+      );
+   }
+   @Get('/about/json')
+   aboutjson(@Res() res: Response) {
+      return res.json(
+      {
+         status: true,
+         message: 'Success',
+         data: [
+            {
+               id: 1,
+               courses: 'Docker',
+               releaseDate: 'Mar 20, 2013'
+            },
+            {
+               id: 2,
+               courses: 'Github',
+               releaseDate: 'Apr 10, 2008'
+            },
+            {
+               id: 3,
+               courses: 'AWS',
+               releaseDate: 'Jul 1, 2002'
+            }
+         ]
+      });
    }
    // getAbout(): string {
    //    return this.appService.getAbout();
